8c40f8cb4c90e22913f908c066e73dac9e8df939
Mishima23 <vassileuski@gmail.com>
2016-10-10 23:14:10 +0300

    form finished
    f

--- js/script.js
+++ js/script.js
@@ -1,15 +1,23 @@
 var link = document.querySelector(".card .btn");
 var popup = document.querySelector(".write-us");
 var close = document.querySelector(".write-us .close");
-var name = popup.querySelector("[name=name]");
+//var name = popup.querySelector("[name=name]");
 
 link.addEventListener("click", function(event) {
 event.preventDefault();
 popup.classList.add("show");
-name.focus();
+//name.focus();
 });
 
 close.addEventListener("click", function(event) {
 event.preventDefault();
 popup.classList.remove("show");
+});
+
+window.addEventListener("keydown", function(event) {
+if (event.keyCode === 27) {
+if (popup.classList.contains("show")) {
+popup.classList.remove("show");
+}
+}
 });
