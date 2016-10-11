2989ffda86a75543f6ccd49388747422dcba23e5
mishima23 <vassileuski@gmail.com>
2016-10-11 12:48:35 +0000

    code formatted

--- js/script.js
+++ js/script.js
@@ -4,20 +4,20 @@ var close = document.querySelector(".write-us .close");
 //var name = popup.querySelector("[name=name]");
 
 link.addEventListener("click", function(event) {
-event.preventDefault();
-popup.classList.add("show");
-//name.focus();
+    event.preventDefault();
+    popup.classList.add("show");
+    //name.focus();
 });
 
 close.addEventListener("click", function(event) {
-event.preventDefault();
-popup.classList.remove("show");
+    event.preventDefault();
+    popup.classList.remove("show");
 });
 
 window.addEventListener("keydown", function(event) {
-if (event.keyCode === 27) {
-if (popup.classList.contains("show")) {
-popup.classList.remove("show");
-}
-}
+    if (event.keyCode === 27) {
+        if (popup.classList.contains("show")) {
+            popup.classList.remove("show");
+        }
+    }
 });
